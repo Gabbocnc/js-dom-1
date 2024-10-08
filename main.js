f09f8180f29e34d23a405224477bc608b2e60367
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-08 16:47:23 +0200

    add comment

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ const offEl = document.getElementById('off');
 
 
 
-//2. Collego un event list al bottone
+/* //2. Collego un event list al bottone
 accendiEl.addEventListener('click', function () {
     offEl.src = './assets/img/yellow_lamp.png';
     accendiEl.innerHTML = 'Spegni'; 
- 
-})
+}) */
 
 
 let lampadaAccesa = false; 
