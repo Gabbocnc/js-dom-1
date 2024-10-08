e410809f3ab80064cbf664982d186e64a1c971ec
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-08 14:27:41 +0200

    add event on first image

---
+++ main.js
@@ -0,0 +1,14 @@
+//1. Seleziono gli elementi nella dom
+const accendiEl = document.getElementById('accendi');
+const offEl = document.getElementById('off');
+const onEl = document.getElementById('on');
+const spegniEl = document.getElementById('spegni');
+
+
+
+// 2. Collego un event list al bottone
+accendiEl.addEventListener('click', function(){
+    offEl.src = onEl.src;
+})
+
+
