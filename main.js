d88d6a3e7e2d361580d7b0e6cabc581127c6ed0e
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-08 15:43:10 +0200

    add bonus task

--- main.js
+++ main.js
@@ -5,10 +5,26 @@ const offEl = document.getElementById('off');
 
 
 
-// 2. Collego un event list al bottone
+//2. Collego un event list al bottone
 accendiEl.addEventListener('click', function () {
     offEl.src = './assets/img/yellow_lamp.png';
     accendiEl.innerHTML = 'Spegni'; 
  
 })
 
+
+let lampadaAccesa = false; 
+
+accendiEl.addEventListener('click', function () {
+    if (lampadaAccesa) {
+        // Condizione : Se la lampadina e' accesa, puoi spegnerla
+        offEl.src = './assets/img/white_lamp.png'; 
+        accendiEl.innerHTML = 'Accendi'; 
+    } else {
+        // Condizione : Se la lampadina e' spenta, puoi accenderla
+        offEl.src = './assets/img/yellow_lamp.png';
+        accendiEl.innerHTML = 'Spegni'; 
+    }
+    //inverto lo stato della lampadina con l operatore !
+    lampadaAccesa = !lampadaAccesa; 
+})
