5a768cccee877ef054c376a12bdae152921b15be
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-08 15:21:22 +0200

    edit code

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
 //1. Seleziono gli elementi nella dom
 const accendiEl = document.getElementById('accendi');
 const offEl = document.getElementById('off');
-const onEl = document.getElementById('on');
-const spegniEl = document.getElementById('spegni');
+
 
 
 
 // 2. Collego un event list al bottone
 accendiEl.addEventListener('click', function () {
-    offEl.src = onEl.src;
-    accendiEl.innerHTML = 'Spegni'
+    offEl.src = 'yellow_lamp.png';
+    accendiEl.innerHTML = 'Spegni';   
 })
-
-spegniEl.addEventListener('click', function () {
-    onEl.src = offEl.src;
-    spegniEl.innerHTML = 'Accendi'
-    
-}) 
 
